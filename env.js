4df330c7c7bc26a744f00dfd1048b655647e750c
hnifmaghfur <hnifmaghfur@gmail.com>
2020-12-19 09:24:14 +0700

    change env.

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-export const API_URI = `http://192.168.1.7:3000/api/v1`;
-export const API_URL = `http://192.168.1.7:3000`;
+export const API_URI = `http://52.90.159.12:3030/api/v1`;
+export const API_URL = `http://52.90.159.12:3030`;
